c02d6f1afae02f70a30656cd6e01b392dd31813e
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-02-08 12:33:41 +0100

    add unregister callback after registering uncaught exception handlers

--- src/registerUncaughtExceptionHandler.ts
+++ src/registerUncaughtExceptionHandler.ts
@@ -1,12 +1,16 @@
 import { LoggerInstance } from 'winston';
 
 export function registerUncaughtExceptionHandler(logger: LoggerInstance) {
-  process.on('uncaughtException', error => {
+  const callback = (error: Error) => {
     logger.error(error.message, error);
 
     const serializedError = JSON.stringify(error);
     if (serializedError !== '{}') {
       logger.debug(serializedError);
     }
-  });
+  };
+
+  process.on('uncaughtException', callback);
+
+  return () => process.removeListener('uncaughtException', callback);
 }
