e8cc9ec164196e79c1f0b9f76e039b38da3f59da
Andrzej Wódkiewicz <akwodkiewicz@gmail.com>
2018-03-09 16:24:58 +0100

    [P][T](WIP) add connect & sendmsg test

--- Player/Player.Tests/PlayerCommunicatorTests.cs
+++ Player/Player.Tests/PlayerCommunicatorTests.cs
@@ -3,17 +3,67 @@ using NUnit;
 using NUnit.Framework;
 using System.Net.Sockets;
 using System.Net;
+using System.Threading.Tasks;
 
 namespace Player.Tests
 {
     [TestFixture]
     class PlayerCommunicatorTests
     {
-        [Test]
-        public void AlwaysPasses()
+        private TcpListener _server;
+        private Task<TcpClient> _acceptTask;
+        private int _port = 12031;
+
+        [SetUp]
+        public void Setup()
         {
-            Assert.That(true);
+            _server = new TcpListener(IPAddress.Loopback, _port);
+            _server.Start();
+            _acceptTask = _server.AcceptTcpClientAsync();
         }
 
+        [TearDown]
+        public void TearDown()
+        {
+            _server.Stop();
+
+        }
+
+        [Test]
+        public async Task ConnectsToServer()
+        {
+            // When
+            var communicator = new Communicator("localhost", _port);
+            var communicatorSeenFromServer = await _acceptTask;
+
+            // Then
+            Assert.That(communicatorSeenFromServer.Connected);
+
+        }
+
+        [Test]
+        public async Task SendMessageSuccess()
+        {
+            // Give
+            var message = "testMessage";
+
+            var communicator = new Communicator("localhost", _port);
+
+            var communicatoSeenFromServer = await _acceptTask;
+            communicator.Send(message);
+
+            var byteStream = communicatoSeenFromServer.GetStream();
+
+            byte[] byteArray = new byte[4];
+            byteStream.Read(byteArray, 0, 4);
+
+            var messageLen = IPAddress.NetworkToHostOrder(BitConverter.ToInt32(byteArray, 0));
+
+            byte[] buffer = new byte[messageLen];
+            byteStream.Read(buffer, 0, messageLen);
+
+            Assert.That(message == System.Text.Encoding.UTF8.GetString(buffer));
+
+        }
     }
 }
