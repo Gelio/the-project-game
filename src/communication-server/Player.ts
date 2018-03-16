7d107a16457162a4b733297e7c1533e23c7548b0
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-03-16 21:58:45 +0100

    [CS] adjust Player to handle multiple GMs

--- src/communication-server/Player.ts
+++ src/communication-server/Player.ts
@@ -3,42 +3,39 @@ import { LoggerInstance } from 'winston';
 import { Communicator } from '../common/Communicator';
 
 import { Message } from '../interfaces/Message';
-import { PlayerAcceptedMessage } from '../interfaces/messages/PlayerAcceptedMessage';
-import { PlayerHelloMessage } from '../interfaces/messages/PlayerHelloMessage';
 import { Service } from '../interfaces/Service';
 
 import { MessageRouter } from './MessageRouter';
+import { PlayerInfo } from './PlayerInfo';
 
 export class Player implements Service {
-  private _isAccepted = false;
-  // @ts-ignore
-  private _id: number;
+  public readonly info: PlayerInfo;
 
   private readonly communicator: Communicator;
   private readonly messageRouter: MessageRouter;
   private readonly logger: LoggerInstance;
 
-  public get isAccepted() {
-    return this._isAccepted;
+  public get id() {
+    return this.info.id;
   }
 
-  public get id(): number {
-    return this._id;
-  }
-
-  constructor(communicator: Communicator, messageRouter: MessageRouter, logger: LoggerInstance) {
+  constructor(
+    communicator: Communicator,
+    messageRouter: MessageRouter,
+    logger: LoggerInstance,
+    playerInfo: PlayerInfo
+  ) {
     this.communicator = communicator;
     this.messageRouter = messageRouter;
     this.logger = logger;
+    this.info = playerInfo;
 
     this.handleMessage = this.handleMessage.bind(this);
-    this.handleMessageSent = this.handleMessageSent.bind(this);
   }
 
   public init() {
     this.communicator.bindListeners();
     this.communicator.on('message', this.handleMessage);
-    this.communicator.on('messageSent', this.handleMessageSent);
   }
 
   public destroy() {
@@ -59,40 +56,7 @@ export class Player implements Service {
       return;
     }
 
-    if (!this._isAccepted) {
-      if (message.type === 'PLAYER_HELLO') {
-        return this.handlePlayerHelloMessage(<any>message);
-      }
-
-      this.logger.warn(
-        `Player ${this._id} tried to send other message than PLAYER_HELLO as handshake`
-      );
-
-      return;
-    }
-
-    this.messageRouter.sendMessageToGameMaster(message);
-  }
-
-  private handlePlayerHelloMessage(message: PlayerHelloMessage) {
-    this._id = message.payload.temporaryId;
-    this.messageRouter.registerPlayerCommunicator(this._id, this.communicator);
-    this.messageRouter.sendMessageToGameMaster(message);
-  }
-
-  private handleMessageSent<T>(message: Message<T>) {
-    if (message.type === 'PLAYER_ACCEPTED') {
-      const acceptedMessage: PlayerAcceptedMessage = <any>message;
-      this._isAccepted = true;
-      this.messageRouter.unregisterPlayerCommunicator(this._id);
-      this._id = acceptedMessage.payload.assignedPlayerId;
-      this.messageRouter.registerPlayerCommunicator(this._id, this.communicator);
-
-      this.communicator.removeListener('messageSent', this.handleMessageSent);
-    } else if (message.type === 'PLAYER_REJECTED') {
-      this.messageRouter.unregisterPlayerCommunicator(this._id);
-      this.destroy();
-    }
+    this.messageRouter.sendMessageToGameMaster(this.info.gameName, message);
   }
 
   private isMessageValid<T>(message: Message<T>) {
