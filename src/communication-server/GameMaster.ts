46d3e50296f8126cbf0fd2fad3f0a77d7fd0fc5b
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-03-16 21:56:52 +0100

    [CS] adjust GameMaster to handle multiple games

--- src/communication-server/GameMaster.ts
+++ src/communication-server/GameMaster.ts
@@ -1,33 +1,51 @@
 import { LoggerInstance } from 'winston';
 
 import { Communicator } from '../common/Communicator';
+import { Message } from '../interfaces/Message';
 import { MessageWithRecipient } from '../interfaces/MessageWithRecipient';
 import { Service } from '../interfaces/Service';
+
+import { Game } from './Game';
 import { MessageRouter } from './MessageRouter';
 
 export class GameMaster implements Service {
-  private readonly communicator: Communicator;
+  public readonly game: Game;
+  public readonly communicator: Communicator;
+
   private readonly messageRouter: MessageRouter;
   private readonly logger: LoggerInstance;
 
-  constructor(communicator: Communicator, messageRouter: MessageRouter, logger: LoggerInstance) {
+  constructor(
+    communicator: Communicator,
+    messageRouter: MessageRouter,
+    logger: LoggerInstance,
+    game: Game
+  ) {
     this.communicator = communicator;
     this.messageRouter = messageRouter;
     this.logger = logger;
+    this.game = game;
 
     this.handleMessage = this.handleMessage.bind(this);
   }
 
   public init() {
-    this.communicator.bindListeners();
-    this.messageRouter.registerGameMasterCommunicator(this.communicator);
+    this.messageRouter.registerGameMasterCommunicator(
+      this.game.gameDefinition.name,
+      this.communicator
+    );
     this.communicator.on('message', this.handleMessage);
   }
 
   public destroy() {
     this.logger.verbose('Destroying GM connection');
+    this.messageRouter.unregisterGameMasterCommunicator(this.game.gameDefinition.name);
+    this.game.destroy();
     this.communicator.destroy();
-    this.messageRouter.unregisterGameMasterCommunicator();
+  }
+
+  public sendMessage(message: Message<any>) {
+    this.communicator.sendMessage(message);
   }
 
   private handleMessage<T>(message: MessageWithRecipient<T>) {
