1a2e0875b3e3f01c52ca6306d7f61cafbf47420b
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-03-17 13:46:19 +0100

    [JS] remove RoundStarted and RoundFinished messages

--- src/interfaces/messages/RoundFinishedMessage.ts
+++
@@ -1,6 +0,0 @@
-import { MessageWithRecipient } from '../MessageWithRecipient';
-
-export interface RoundFinishedMessage extends MessageWithRecipient<{ winTeamId: number }> {
-  type: 'ROUND_FINISHED';
-  senderId: -1;
-}
