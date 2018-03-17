65e6a63439723d4293583c8d03ec83fca8fceadf
Gustaw Żyngiel <gustaw.zyngiel@gmail.com>
2018-03-17 16:52:54 +0100

    [P][T] WaitForGameStart

--- Player/Player.Tests/PlayerTests.cs
+++ Player/Player.Tests/PlayerTests.cs
@@ -5,6 +5,8 @@ using NUnit;
 using NUnit.Framework;
 using Moq;
 using Player.Common;
+using Player.Messages;
+using Newtonsoft.Json;
 
 namespace Player.Tests
 {
@@ -12,11 +14,21 @@ namespace Player.Tests
     class PlayerTests
     {
         Mock<ICommunicator> _communicator;
+        PlayerConfig _playerConfig;
 
         [SetUp]
         public void Setup()
         {
             _communicator = new Mock<ICommunicator>();
+            _playerConfig = new PlayerConfig
+            {
+                AskLevel = 10,
+                RespondLevel = 10,
+                Timeout = 11,
+                GameName = "asdfasdf",
+                TeamNumber = 1
+            };
+
         }
 
         [Test]
@@ -26,15 +38,7 @@ namespace Player.Tests
             string expectedMessage = Consts.PLAYER_ACCEPTED;
             _communicator.Setup(x => x.Receive()).Returns(expectedMessage);
 
-            var playerConfig = new PlayerConfig
-            {
-                AskLevel = 10,
-                RespondLevel = 10,
-                Timeout = 11,
-                GameName = "asdfasdf"
-            };
-
-            var player = new Player(_communicator.Object, playerConfig);
+            var player = new Player(_communicator.Object, _playerConfig);
 
             // When
             player.ConnectToServer();
@@ -49,18 +53,52 @@ namespace Player.Tests
             // Give
             string expectedMessage = Consts.PLAYER_REJECTED;
             _communicator.Setup(x => x.Receive()).Returns(expectedMessage);
-            var playerConfig = new PlayerConfig
-            {
-                AskLevel = 10,
-                RespondLevel = 10,
-                Timeout = 11,
-                GameName = "asdfasdf"
-            };
 
-            var player = new Player(_communicator.Object, playerConfig);
+            var player = new Player(_communicator.Object, _playerConfig);
 
             Assert.Throws<PlayerRejectedException>(() => player.ConnectToServer());
         }
 
+        [Test]
+        public void WaitForGameStartSucceeds()
+        {
+            // Give
+            var expectedTeamMembersIds = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+            var expectedLeaderId = 3;
+            var message = new Message<GameStartedPayload>
+            {
+                Type = Common.Consts.GameStarted,
+                Payload = new GameStartedPayload
+                {
+                    TeamInfo = new Dictionary<int, TeamInfo>
+                    {
+                        {1, new TeamInfo
+                            {
+                                LeaderId = expectedLeaderId,
+                                Players = expectedTeamMembersIds
+                            }
+                        },
+
+                        {2, new TeamInfo
+                            {
+                                LeaderId = 9,
+                                Players = new List<int> {9,10,11,12}
+                            }
+                        }
+                    }
+                }
+            };
+            string expectedMessage = JsonConvert.SerializeObject(message);
+            _communicator.Setup(x => x.Receive()).Returns(expectedMessage);
+            var player = new Player(_communicator.Object, _playerConfig);
+
+            // When
+            var result = player.WaitForGameStart();
+
+            //Then
+            Assert.That(result, Is.True);
+            Assert.That(player.LeaderId, Is.EqualTo(expectedLeaderId));
+            Assert.That(player.TeamMembersIds, Is.EquivalentTo(expectedTeamMembersIds));
+        }
     }
 }
