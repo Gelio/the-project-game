14362680debb7644a5a5285827d3ec6c6656cc5e
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-03-17 13:25:49 +0100

    [JS] adjust PlaceDownPieceResponse to include didCompleteGoal

--- src/interfaces/responses/PlaceDownPieceResponse.ts
+++ src/interfaces/responses/PlaceDownPieceResponse.ts
@@ -1,6 +1,11 @@
 import { MessageWithRecipient } from '../MessageWithRecipient';
 
-export interface PlaceDownPieceResponse extends MessageWithRecipient<undefined> {
+export interface PlaceDownPieceResponsePayload {
+  didCompleteGoal?: boolean;
+}
+
+export interface PlaceDownPieceResponse
+  extends MessageWithRecipient<PlaceDownPieceResponsePayload> {
   type: 'PLACE_DOWN_PIECE_RESPONSE';
   senderId: -1;
 }
