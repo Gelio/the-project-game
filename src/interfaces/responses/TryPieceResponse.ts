fb246277e787d08f1cc10010650bfa89e7479f77
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-03-17 13:25:18 +0100

    [JS] remove TryPiece request and response

--- src/interfaces/responses/TryPieceResponse.ts
+++
@@ -1,7 +0,0 @@
-import { MessageWithRecipient } from '../MessageWithRecipient';
-
-export interface TryPieceResponse
-  extends MessageWithRecipient<{ isSham: boolean; didCompleteGoal: boolean }> {
-  type: 'TRY_PIECE_RESPONSE';
-  senderId: -1;
-}
