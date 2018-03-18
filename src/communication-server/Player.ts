d10924c3c33678b76b58717c877d3edf24dafe00
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-03-18 13:41:44 +0100

    [CS] split destroying Player by reason, add Player events
    Either because GM/Player disconnected or because the game
    has been finished

--- src/communication-server/Player.ts
+++ src/communication-server/Player.ts
@@ -1,17 +1,17 @@
 import { LoggerInstance } from 'winston';
 
 import { Communicator } from '../common/Communicator';
+import { CustomEventEmitter } from '../common/CustomEventEmitter';
 
 import { Message } from '../interfaces/Message';
-import { Service } from '../interfaces/Service';
 
 import { MessageRouter } from './MessageRouter';
 import { PlayerInfo } from './PlayerInfo';
 
-export class Player implements Service {
+export class Player extends CustomEventEmitter {
+  public readonly communicator: Communicator;
   public readonly info: PlayerInfo;
 
-  private readonly communicator: Communicator;
   private readonly messageRouter: MessageRouter;
   private readonly logger: LoggerInstance;
 
@@ -25,29 +25,53 @@ export class Player implements Service {
     logger: LoggerInstance,
     playerInfo: PlayerInfo
   ) {
+    super();
+
     this.communicator = communicator;
     this.messageRouter = messageRouter;
     this.logger = logger;
     this.info = playerInfo;
 
     this.handleMessage = this.handleMessage.bind(this);
+    this.destroy = this.destroy.bind(this);
   }
 
   public init() {
     this.messageRouter.registerPlayerCommunicator(this.id, this.communicator);
     this.communicator.on('message', this.handleMessage);
+    this.communicator.once('destroy', this.destroy);
   }
 
+  /**
+   * Invoked when the player or the GM that hosts the game disconnects
+   */
   public destroy() {
-    this.logger.verbose(`Destroying player ${this.id}`);
+    this.unbindListeners();
+    this.logger.verbose(`Destroying player ${this.id} and disconnecting his/her communicator`);
 
+    this.tryUnregisterFromMessageRouter();
+    this.communicator.destroy();
+
+    this.emit('destroy');
+    this.removeAllListeners();
+  }
+
+  public onGameFinished() {
+    this.unbindListeners();
+    this.logger.verbose(`Destroying player ${this.id} because the game finished`);
+    this.tryUnregisterFromMessageRouter();
+
+    this.emit('gameFinish');
+    this.removeAllListeners();
+  }
+
+  private tryUnregisterFromMessageRouter() {
     if (this.messageRouter.hasRegisteredPlayerCommunicator(this.id)) {
       this.messageRouter.unregisterPlayerCommunicator(this.id);
     }
-    this.communicator.destroy();
   }
 
-  private handleMessage<T>(message: Message<T>) {
+  private handleMessage(message: Message<any>) {
     if (!this.isMessageValid(message)) {
       this.logger.warn(
         `Received message with sender ID ${message.senderId} but player ID is ${this.id}`
@@ -59,7 +83,12 @@ export class Player implements Service {
     this.messageRouter.sendMessageToGameMaster(this.info.gameName, message);
   }
 
-  private isMessageValid<T>(message: Message<T>) {
+  private isMessageValid(message: Message<any>) {
     return this.id === message.senderId;
+  }
+
+  private unbindListeners() {
+    this.communicator.removeListener('message', this.handleMessage);
+    this.communicator.removeListener('destroy', this.destroy);
   }
 }
