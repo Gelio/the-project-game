13bb7f895ced1583cc43432483c36be92bbaff88
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-03-24 18:10:50 +0100

    [GM] add handler for DeletePieceRequest

--- src/game-master/game-logic/handleDeletePieceRequest.ts
+++ src/game-master/game-logic/handleDeletePieceRequest.ts
@@ -1,18 +1,32 @@
 import { createDelay } from '../../common/createDelay';
+
+import { ActionDelays } from '../../interfaces/ActionDelays';
 import { DeletePieceRequest } from '../../interfaces/requests/DeletePieceRequest';
 import { DeletePieceResponse } from '../../interfaces/responses/DeletePieceResponse';
+
 import { Board } from '../models/Board';
+
 import { Player } from '../Player';
 import { PlayersContainer } from '../PlayersContainer';
 import { ProcessMessageResult } from '../ProcessMessageResult';
 
 export function handleDeletePieceRequest(
-  // TODO: add delays
   board: Board,
-  playersContainer: PlayersContainer,
+  _playersContainer: PlayersContainer,
+  actionDelays: ActionDelays,
   sender: Player,
-  deletePieceRequest: DeletePieceRequest
+  _deletePieceRequest: DeletePieceRequest
 ): ProcessMessageResult<DeletePieceResponse> {
+  if (!sender.heldPiece) {
+    return {
+      valid: false,
+      reason: 'Player does not hold a piece'
+    };
+  }
+
+  board.removePiece(sender.heldPiece);
+  sender.heldPiece = null;
+
   const response: DeletePieceResponse = {
     type: 'DELETE_PIECE_RESPONSE',
     payload: undefined,
@@ -20,15 +34,11 @@ export function handleDeletePieceRequest(
     senderId: -1
   };
 
-  const responsePromise: Promise<DeletePieceResponse> = createDelay(500).then(() => {
-    sender.isBusy = false;
-
-    return response;
-  });
+  const responsePromise = createDelay(actionDelays.destroy).then(() => response);
 
   return {
     valid: true,
-    delay: 500,
+    delay: actionDelays.destroy,
     responseMessage: responsePromise
   };
 }
