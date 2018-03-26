9bf5c9fe208ab2c60f4a7f97e70e648683bd166b
Anastasia <khlebousa@gmail.com>
2018-03-26 11:16:28 +0200

    [GM] add DiscoveryRequest handler

--- src/game-master/game-logic/handleDiscoveryRequest.ts
+++ src/game-master/game-logic/handleDiscoveryRequest.ts
@@ -1,16 +1,62 @@
 import { createDelay } from '../../common/createDelay';
+import { Point } from '../../common/Point';
 
 import { DiscoveryRequest } from '../../interfaces/requests/DiscoveryRequest';
-import { DiscoveryResponse } from '../../interfaces/responses/DiscoveryResponse';
+import { DiscoveryResponse, TileInfo } from '../../interfaces/responses/DiscoveryResponse';
 
 import { Player } from '../Player';
 import { ProcessMessageResult } from '../ProcessMessageResult';
 import { MessageHandlerDependencies } from './MessageHandlerDependencies';
 
+import { Board } from '../models/Board';
+import { Piece } from '../models/Piece';
+
+function getDistanceToPiece(tilePosition: Point, piece: Piece): number {
+  return Math.abs(tilePosition.x - piece.position.x) + Math.abs(tilePosition.y - piece.position.y);
+}
+
+function getDistanceToClosestPiece(tilePosition: Point, pieces: Piece[]): number {
+  if (pieces.length === 0) {
+    return -1;
+  }
+  let minDistance: number = Infinity;
+  for (const p of pieces) {
+    const distance = getDistanceToPiece(tilePosition, p);
+    if (minDistance > distance) {
+      minDistance = distance;
+    }
+  }
+
+  return minDistance;
+}
+
+function getCorrectTiles(playerPosition: Point, board: Board): TileInfo[] {
+  const outTiles: TileInfo[] = [];
+  const fromX = Math.max(0, playerPosition.x - 1);
+  const fromY = Math.max(0, playerPosition.y - 1);
+  const toX = Math.min(board.size.x - 1, playerPosition.x + 1);
+  const toY = Math.min(board.size.goalArea * 2 + board.size.taskArea - 1, playerPosition.y + 1);
+
+  for (let i = fromX; i <= toX; i++) {
+    for (let j = fromY; j <= toY; j++) {
+      const tile = board.getTileAtPosition({ x: i, y: j });
+      outTiles.push({
+        x: i,
+        y: j,
+        playerId: tile.player === null ? null : tile.player.playerId,
+        piece: tile.piece === null ? false : true,
+        distanceToClosestPiece: getDistanceToClosestPiece({ x: tile.x, y: tile.y }, board.pieces)
+      });
+    }
+  }
+
+  return outTiles;
+}
+
 export function handleDiscoveryRequest(
   { board, actionDelays }: MessageHandlerDependencies,
   sender: Player,
-  _deletePieceRequest: DiscoveryRequest
+  _discoveryRequest: DiscoveryRequest
 ): ProcessMessageResult<DiscoveryResponse> {
   if (!sender.position) {
     return {
@@ -20,16 +66,16 @@ export function handleDiscoveryRequest(
   }
 
   const playerPosition = sender.position;
-  // const fromX = Math.min(playerPosition.)
 
-  const response: DiscoveryResponse = {
-    type: 'DELETE_PIECE_RESPONSE',
-    payload: undefined,
+  const responsePromise = createDelay(actionDelays.destroy).then((): DiscoveryResponse => ({
+    type: 'DISCOVERY_RESPONSE',
+    payload: {
+      timestamp: Date.now(),
+      tiles: getCorrectTiles(playerPosition, board)
+    },
     recipientId: sender.playerId,
     senderId: -1
-  };
-
-  const responsePromise = createDelay(actionDelays.destroy).then(() => response);
+  }));
 
   return {
     valid: true,
