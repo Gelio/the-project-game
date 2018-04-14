5c3cc83777416d747589ec635a72314f6d4d80ea
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-04-14 17:59:12 +0200

    [GM} add handling place down piece requests in message handler

--- src/game-master/game-logic/PlayerMessageHandler.ts
+++ src/game-master/game-logic/PlayerMessageHandler.ts
@@ -7,6 +7,7 @@ import { ProcessMessageResult } from '../ProcessMessageResult';
 
 import { handleDeletePieceRequest } from './handleDeletePieceRequest';
 import { handleDiscoveryRequest } from './handleDiscoveryRequest';
+import { handlePlaceDownPieceRequest } from './handlePlaceDownPieceRequest';
 import { handleRefreshStateRequest } from './handleRefreshStateRequest';
 import { MessageHandlerDependencies } from './MessageHandlerDependencies';
 
@@ -17,6 +18,7 @@ export class PlayerMessageHandler {
     // [REQUEST_TYPE.COMMUNICATION_REQUEST]: handleCommunicationRequest,
     [REQUEST_TYPE.DELETE_PIECE_REQUEST]: handleDeletePieceRequest,
     [REQUEST_TYPE.DISCOVERY_REQUEST]: handleDiscoveryRequest,
+    [REQUEST_TYPE.PLACE_DOWN_PIECE_REQUEST]: handlePlaceDownPieceRequest,
     [REQUEST_TYPE.REFRESH_STATE_REQUEST]: handleRefreshStateRequest
   };
 
