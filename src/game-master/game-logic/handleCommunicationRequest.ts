39db5078766b47a4422882394c569ca66a33ed2f
Anastasia <khlebousa@gmail.com>
2018-04-24 20:36:53 +0200

    [GM] handle communication request

---
+++ src/game-master/game-logic/handleCommunicationRequest.ts
@@ -0,0 +1,59 @@
+import { createDelay } from '../../common/createDelay';
+import { GAME_MASTER_ID } from '../../common/EntityIds';
+
+import { CommunicationRequestsStore } from '../communication/CommunicationRequestsStore';
+import { Player } from '../Player';
+import { ProcessMessageResult } from '../ProcessMessageResult';
+
+import { MessageHandlerDependencies } from './MessageHandlerDependencies';
+
+import { RequestSentMessage } from '../../interfaces/messages/RequestSentMessage';
+import {
+  CommunicationRequestFromSender,
+  CommunicationRequestToRecipient
+} from '../../interfaces/requests/CommunicationRequest';
+
+export function handleCommunicationRequest(
+  communicationRequestsStore: CommunicationRequestsStore,
+  { actionDelays, sendMessage }: MessageHandlerDependencies,
+  sender: Player,
+  communicationRequest: CommunicationRequestFromSender
+): ProcessMessageResult<RequestSentMessage> {
+  const senderId = sender.playerId;
+  const recipientId = communicationRequest.payload.targetPlayerId;
+
+  if (communicationRequestsStore.isRequestPending(senderId, recipientId) === true) {
+    return {
+      valid: false,
+      reason: `Communication request to player ${recipientId} is pending`
+    };
+  }
+
+  communicationRequestsStore.addPendingRequest(senderId, recipientId);
+
+  const communicationRequstToRecipient: CommunicationRequestToRecipient = {
+    type: 'COMMUNICATION_REQUEST',
+    payload: { senderPlayerId: senderId },
+    recipientId: recipientId,
+    senderId: GAME_MASTER_ID
+  };
+
+  const responsePromise = createDelay(actionDelays.communicationRequest).then(
+    (): RequestSentMessage => {
+      sendMessage(communicationRequstToRecipient);
+
+      return {
+        type: 'REQUEST_SENT',
+        payload: undefined,
+        recipientId: senderId,
+        senderId: GAME_MASTER_ID
+      };
+    }
+  );
+
+  return {
+    valid: true,
+    delay: actionDelays.communicationRequest,
+    responseMessage: responsePromise
+  };
+}
