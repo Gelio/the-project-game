f3e12b6852a3efe15579e82c6fcb33664d3a1d5d
Andrzej Wódkiewicz <akwodkiewicz@gmail.com>
2018-05-10 08:50:44 +0200

    [P] annotate properties with camelCase names

--- Player/Player/Messages/DTO/TileCommunicationDTO.cs
+++ Player/Player/Messages/DTO/TileCommunicationDTO.cs
@@ -1,19 +1,32 @@
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Player.Messages.DTO
 {
     public class TileCommunicationDTO
     {
+        [JsonProperty("distanceToPiece")]
         public int DistanceToPiece;
+
+        [JsonProperty("hasCompletedGoal")]
         public bool HasCompletedGoal;
+
+        [JsonProperty("piece")]
         public PieceDTO Piece;
+
+        [JsonProperty("playerId")]
         public string PlayerId;
+
+        [JsonProperty("timeStamp")]
         public long TimeStamp;
     }
 
     public class PieceDTO
     {
+        [JsonProperty("isSham")]
         public bool IsSham;
+
+        [JsonProperty("wasTested")]
         public bool WasTested;
     }
 }
