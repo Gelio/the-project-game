9ed9ab65b81d59dfa50392dfa62d6ce4778327e2
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-05-13 22:19:13 +0200

    [CS] fix CS startup script

--- src/start-communication-server.ts
+++ src/start-communication-server.ts
@@ -1,22 +1,29 @@
+import { getMessageValidator } from './common/getMessageValidator';
 import { LoggerFactory } from './common/logging/LoggerFactory';
+
 import { CommunicationServer } from './communication-server/CommunicationServer';
 import { MessageRouter } from './communication-server/MessageRouter';
 
 // tslint:disable-next-line:no-require-imports no-var-requires
 const config = require('./communication-server.config.json');
 
-const messageRouter = new MessageRouter();
+(async () => {
+  const messageRouter = new MessageRouter();
 
-const loggerFactory = new LoggerFactory();
-const consoleLogger = loggerFactory.createConsoleLogger();
+  const loggerFactory = new LoggerFactory();
+  const consoleLogger = loggerFactory.createConsoleLogger();
 
-const communicationServer = new CommunicationServer(
-  {
-    hostname: config.hostname,
-    port: config.port
-  },
-  messageRouter,
-  consoleLogger
-);
+  const messageValidator = await getMessageValidator();
 
-communicationServer.init();
+  const communicationServer = new CommunicationServer(
+    {
+      hostname: config.hostname,
+      port: config.port
+    },
+    messageRouter,
+    consoleLogger,
+    messageValidator
+  );
+
+  communicationServer.init();
+})();
