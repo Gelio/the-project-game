bef5375e139be18c1363003f62a02398172a6037
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-05-16 15:22:02 +0200

    [CS] fix schema errors filtering

--- src/common/logging/stringifySchemaValidationErrors.ts
+++ src/common/logging/stringifySchemaValidationErrors.ts
@@ -2,7 +2,7 @@ import * as ajv from 'ajv';
 
 export function stringifySchemaValidationErrors(errors: ajv.ErrorObject[]): string {
   const filteredErrors = errors.filter(
-    error => error.keyword !== 'const' && error.dataPath !== '.type'
+    error => error.keyword !== 'const' || error.dataPath !== '.type'
   );
 
   return JSON.stringify(filteredErrors, null, 2);
