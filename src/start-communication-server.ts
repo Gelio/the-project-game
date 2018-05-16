30c25db67931ade57f4f66475329be570df0d621
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-05-16 16:11:49 +0200

    [CS] exract shared arguments, refactor code

--- src/start-communication-server.ts
+++ src/start-communication-server.ts
@@ -1,4 +1,5 @@
 import * as ajv from 'ajv';
+import { ArgumentParser } from 'argparse';
 
 import { getMessageValidator } from './common/getMessageValidator';
 import { LoggerFactory } from './common/logging/LoggerFactory';
@@ -6,13 +7,32 @@ import { LoggerFactory } from './common/logging/LoggerFactory';
 import { CommunicationServer } from './communication-server/CommunicationServer';
 import { MessageRouter } from './communication-server/MessageRouter';
 
+import { CSArguments } from './interfaces/arguments/CSArguments';
+
+import { addSharedArguments } from './arguments/addSharedArguments';
+import { getLogLevel } from './arguments/getLogLevel';
+
 // tslint:disable-next-line:no-require-imports no-var-requires
 const config = require('./communication-server.config.json');
 
+function parseCSArguments(): CSArguments {
+  const parser = new ArgumentParser({
+    description: 'Communication server',
+    addHelp: true
+  });
+
+  addSharedArguments(parser);
+
+  return parser.parseArgs();
+}
+
 (async () => {
   const messageRouter = new MessageRouter();
+  const parsedArguments = parseCSArguments();
 
   const loggerFactory = new LoggerFactory();
+  loggerFactory.logLevel = getLogLevel(parsedArguments);
+
   const consoleLogger = loggerFactory.createConsoleLogger();
 
   let messageValidator: ajv.ValidateFunction;
