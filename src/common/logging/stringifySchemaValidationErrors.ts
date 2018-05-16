4d75f30daf86ce87b5f554a4ad3de298f737685e
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-05-16 15:09:22 +0200

    [JS] add schema validation errors validation

---
+++ src/common/logging/stringifySchemaValidationErrors.ts
@@ -0,0 +1,9 @@
+import * as ajv from 'ajv';
+
+export function stringifySchemaValidationErrors(errors: ajv.ErrorObject[]): string {
+  const filteredErrors = errors.filter(
+    error => error.keyword !== 'const' && error.dataPath !== '.type'
+  );
+
+  return JSON.stringify(filteredErrors, null, 2);
+}
