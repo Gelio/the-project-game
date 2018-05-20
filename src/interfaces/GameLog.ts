2030effbe3e0f9596f496b891d73515bfaaf61f9
proszewskip <proszewskip@student.mini.pw.edu.pl>
2018-05-20 23:54:35 +0200

    [GM] change interface to class

--- src/interfaces/GameLog.ts
+++ src/interfaces/GameLog.ts
@@ -1,16 +1,35 @@
 import { EntityId } from '../common/EntityIds';
+import { TeamId } from '../common/TeamId';
 
-export enum role {
+export enum PlayerRole {
   Member = 'member',
   Leader = 'leader'
 }
 
-export interface GameLog {
-  type: string;
-  timestamp: number;
-  playerId: EntityId;
-  teamId: number;
-  round: number;
-  role: role;
-  valid: number;
+export class GameLog {
+  public readonly type: string;
+  public readonly timestamp: string;
+  public readonly playerId: EntityId;
+  public readonly teamId: TeamId;
+  public readonly round: number;
+  public readonly role: PlayerRole;
+  public readonly valid: boolean;
+
+  constructor(
+    type: string,
+    timestamp: string,
+    playerId: EntityId,
+    teamId: TeamId,
+    round: number,
+    role: PlayerRole,
+    valid: boolean
+  ) {
+    this.teamId = teamId;
+    this.type = type;
+    this.valid = valid;
+    this.timestamp = timestamp;
+    this.playerId = playerId;
+    this.round = round;
+    this.role = role;
+  }
 }
