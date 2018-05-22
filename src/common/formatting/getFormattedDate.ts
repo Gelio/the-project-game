7cf224055e51fecfc1475b27b22c39631bafc1da
proszewskip <proszewskip@student.mini.pw.edu.pl>
2018-05-22 19:13:56 +0200

    [JS] add leading zeros to formatted date

--- src/common/formatting/getFormattedDate.ts
+++ src/common/formatting/getFormattedDate.ts
@@ -1,3 +1,11 @@
 export function getFormattedDate(date: Date): string {
-  return `${date.getFullYear()}-${date.getMonth()}-${date.getDay()}-${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}`;
+  const year = date.getFullYear().toString();
+  const month = `${date.getMonth()}`.padStart(2, '0');
+  const day = `${date.getDay()}`.padStart(2, '0');
+  const hour = `${date.getHours()}`.padStart(2, '0');
+  const minutes = `${date.getMinutes()}`.padStart(2, '0');
+  const seconds = `${date.getSeconds()}`.padStart(2, '0');
+  const array = [year, month, day, hour, minutes, seconds];
+
+  return array.join('-');
 }
