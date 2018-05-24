fe965f3785de144b427773a4ddab8977f473264a
proszewskip <proszewskip@student.mini.pw.edu.pl>
2018-05-24 10:07:16 +0200

    [JS] fix getFormattedDate to show correct month

--- src/common/formatting/getFormattedDate.ts
+++ src/common/formatting/getFormattedDate.ts
@@ -1,7 +1,7 @@
 export function getFormattedDate(date: Date): string {
   const year = date.getFullYear().toString();
-  const month = `${date.getMonth()}`.padStart(2, '0');
-  const day = `${date.getDay()}`.padStart(2, '0');
+  const month = `${date.getMonth() + 1}`.padStart(2, '0');
+  const day = `${date.getDay() + 1}`.padStart(2, '0');
   const hour = `${date.getHours()}`.padStart(2, '0');
   const minutes = `${date.getMinutes()}`.padStart(2, '0');
   const seconds = `${date.getSeconds()}`.padStart(2, '0');
